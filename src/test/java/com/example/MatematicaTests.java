a3f3121a89ef443e658e8dc68f92f998245ade7c
vladinski12 <vladalexandrupirvan@gmail.com>
2026-01-11 17:26:43 +0200

    init

---
+++ src/test/java/com/example/MatematicaTests.java
@@ -0,0 +1,44 @@
+package com.example;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MatematicaTests {
+
+    Matematica matematica;
+
+    @Before
+    public void setUp() {
+        matematica = new Matematica();
+    }
+
+    @Test
+    public void testSuma() {
+        int rezultat = matematica.suma(3, 12);
+        int rezultatAsteptat = 15;
+        assertEquals(rezultatAsteptat, rezultat);
+    }
+
+    @Test
+    public void testRaportCorect() {
+        double rezultat = matematica.raport(12, 3);
+        double rezultatAsteptat = 4;
+        assertEquals(rezultatAsteptat, rezultat, 0.1);
+    }
+
+    @Test
+
+    public void testRaportExceptie() {
+        try {
+            matematica.raport(
+                    34, 0);
+            fail("Metoda nu arunca exceptie");
+
+        } catch (IllegalArgumentException er) {
+
+        }
+
+    }
+}
